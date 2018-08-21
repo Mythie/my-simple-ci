da81e31ae73575f2d3c7ee7898922d3e2fc7c663
Lucas Smith <mythiedev@gmail.com>
2018-08-21 14:58:18 +1000

    Minor tweaks

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   console.log('Now listening on port 3000');
 });
