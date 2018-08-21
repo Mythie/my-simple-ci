8e1198296013f3ff4b11cc5bfde0dc311553589f
Lucas Smith <mythiedev@gmail.com>
2018-08-21 15:06:01 +1000

    woops

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(process.env.PORT, function() {
+app.listen((port, function() {
   console.log('Now listening on port 3000');
 });
